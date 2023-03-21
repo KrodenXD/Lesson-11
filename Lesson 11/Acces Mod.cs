c0093c64a609cd75a1d6d53fb214620192f1bacf
alexc <alexc@SONY-VAIO>
2023-03-21 22:38:06 +0400

    Add project files.

---
+++ Lesson 11/Acces Mod.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_11
+{
+    class Acces_Mod
+    {
+        private int a;
+        private protected int b;
+        protected int c;
+        internal int d;
+        protected internal int e;
+        public int f;
+    }
+}
---
+++ Lesson 11/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Acces_Mod acces_mod = new Acces_Mod();
+            Console.WriteLine(acces_mod.a);
+            Console.WriteLine(acces_mod.b);
+            Console.WriteLine(acces_mod.c);
+            Console.WriteLine(acces_mod.d);
+            Console.WriteLine(acces_mod.e);
+            Console.WriteLine(acces_mod.f);
+        }
+    }
+}
